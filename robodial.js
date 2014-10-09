94701e8425ce61d307287b438b3a1c32018ccc14
andy jiang <andyjiang@gmail.com>
2014-10-08 18:19:14 -0700

    push to heroku

---
+++ robodial.js
@@ -0,0 +1,33 @@
+// This script is going to run every day at a certain time.
+
+// Twilio related stuff.
+var client = require('twilio')(process.env.TWILIO_ASID, process.env.TWILIO_AUTH_TOKEN);
+var twilio_number = process.env.TWILIO_NUMBER;
+
+// Verizon phone numbers.
+var targets = [
+  "(415) 402-0640",
+  "(415) 984-0360",
+  "(415) 228-1100",
+  "(415) 351-1700",
+  "(415) 896-4534",
+  "(415) 680-1022",
+  "(415) 863-3665",
+  "(415) 695-8400",
+  "(415) 504-9688",
+  "(415) 664-5777",
+  "(415) 683-2449",
+  "(415) 504-6093",
+  "(650) 991-9901"
+];
+
+// Loop through and call everyone.
+targets.map(function(number) {
+  client.calls.create({
+    url: "/doyouhaveaniphone6plus",
+    to: number,
+    from: twilio_number
+  }, function(err, call) {
+    console.log(err);
+  });
+});
--- server.js
+++ server.js
@@ -7,11 +7,7 @@ var app = express();
 var server = http.createServer(app);
 var port = process.env.PORT || 3000;
 var twimlBuilder = require('twilio');
-var moment = require('moment');
 
-// Our phone numbers. ==========================================================
-var sales = '2409887757'; // Raph
-var media = '2409887757'; // Diana
 var settings = {          // twilio settings.
   voice: 'woman',
   language: 'en-US'
@@ -29,54 +25,22 @@ server.listen(port, function() {
 	console.log("App is now listening on port " + port);
 });
 
+// TwiML servers. ==============================================================
+app.get('/doyouhaveaniphone6plus', function(req, res) {
+  var twiml = new twimlBuilder.TwimlResponse();
+
+  // TwiML: "Wait a few seconds, sendDigits "3" for sales, wait until it connects,
+  // then ask if they have an iPhone 6 Plus. Record the answer.
+  twiml.play('URL', { digits: 'wwww3' })
+       .record({ timeout: 3, maxLength: 15, action: '/api/recording' });
+
+  res.set('Content-Type', 'text/xml');
+  res.send(twiml.toString());
+});
+
 // API routes. =================================================================
-// Main phone line will post here.
-app.post('/', function(req, res) {
-  var twiml = new twimlBuilder.TwimlResponse();
+app.post('/api/recording', function(req, res) {
+  console.log(req.body);
 
-  // Check if within business hours.
-  if (businessHours()) {
-    twiml.say('Welcome to Segment!', settings)
-         .gather({
-            action: '/ivr',
-            numDigits: 1
-         }, function() {
-            this.say('If you\'re a new or prospective customer and would like more information, please press 1.', settings)
-                .say('if you\'re a member of the media, please press 2', settings)
-                .say('If you\'re calling from a company that is interested in becoming a part of the Segment platform, please email "integrations@segment.com"', settings)
-                .say('if you\'d like technical support, please email "friends@segment.com". To repeat this, press 3."', settings);
-         });
-  } else {
-    twiml.say('Welcome to Segment! Sorry, nobody is here to answer your call. Please try again between Monday and Friday from 9am to 6pm pacific time. Thank you.', settings);
-  }
-
-  res.set('Content-Type', 'text/xml');
-  res.send(twiml.toString());
+  res.send(200);
 });
-
-// Detect caller selection here.
-app.post('/ivr', function(req, res) {
-  var input = req.body.Digits || 3;
-  var twiml = new twimlBuilder.TwimlResponse();
-  var forwardingNumber = process.env.TWILIO_TOLL_NUMBER;
-
-  if (input == 1)
-    forwardingNumber = sales;
-  if (input == 2)
-    forwardingNumber = media;
-
-  twiml.dial(function() {
-    this.number(forwardingNumber);
-  });
-
-  res.set('Content-Type', 'text/xml');
-  res.send(twiml.toString());
-});
-
-// Helper function to determine if now is within business hours.
-function businessHours() {
-  var now = moment();
-  if (now.hour() >= 9 && now.hour() <= 6 && now.day >= 1 && now.day <= 5)
-    return true;
-  return false;
-}
